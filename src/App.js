33d56dd5d9696177de11597885f127184d3b93cf
Mozhganameri <86070437+Mozhganameri@users.noreply.github.com>
2021-12-06 02:23:24 +0330

    files created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./styles.css";
+import Search from "./Search";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Search />
+    </div>
+  );
+}
